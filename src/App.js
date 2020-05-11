6210a6bc703874499e01eb30f987161b977698a7
akpante3 <obije.victor5@gmail.com>
2020-05-11 16:10:34 +0100

    canon

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import './App.scss';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import { getCanvasPosition } from './utils/formulas';
+import Canvas from './component/Canvas';
 
+class App extends Component {
+  componentDidMount() {
+    const self = this;
+    setInterval(() => {
+        self.props.moveObjects(self.canvasMousePosition);
+    }, 10);
+  }
 
-class App extends React.Component {
+  trackMouse(event) {
+    this.canvasMousePosition = getCanvasPosition(event);
+  }
 
-  render () {
+  render() {
     return (
-      <div>
-         hello
-      </div>
+      <Canvas
+        angle={this.props.angle}
+        trackMouse={event => (this.trackMouse(event))}
+      />
     );
   }
 }
+
+App.propTypes = {
+  angle: PropTypes.number.isRequired,
+  moveObjects: PropTypes.func.isRequired,
+};
 
 export default App;
